4a9c347a02915e2b610eba0928a5624618d8f5ab
ฐิตา ชูเมือง <thitachoomuang@gmail.com>
2022-03-31 20:55:54 +0700

    prep for deploy นาทีที่ 38.43

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -117,7 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
+STATIC_URL = 'dj-static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -129,3 +134,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ]
 }
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000'
+]
